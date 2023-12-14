5f652c77a5a30616622f6dfcb962d242d2215248
godchenz <64021799@up.ac.th>
2023-12-14 16:23:29 +0700

    add house doctype

---
+++ sridonchai/sridonchai/doctype/house/test_house.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, university of phayao and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestHouse(FrappeTestCase):
+	pass
