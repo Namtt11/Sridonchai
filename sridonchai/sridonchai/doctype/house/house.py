8dd5fcccebcc756451bd18f2cb4e4810a62514dd
godchenz <64021799@up.ac.th>
2023-12-14 16:48:09 +0700

    update house doctype

--- sridonchai/sridonchai/doctype/house/house.py
+++ sridonchai/sridonchai/doctype/house/house.py
@@ -6,4 +6,17 @@ from frappe.model.document import Document
 
 
 class House(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		house_number: DF.Data | None
+		owner_name: DF.Data | None
+		text: DF.TextEditor | None
+	# end: auto-generated types
+
 	pass
