fd8a55a1d4c8cfba6bacaf7f5b30d0184db6f36d
godchenz <64021799@up.ac.th>
2023-12-21 18:02:54 +0700

    create Monthly doctype

---
+++ sridonchai/sridonchai/doctype/monthly/monthly.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, university of phayao and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Monthly", {
+// 	refresh(frm) {
+
+// 	},
+// });
