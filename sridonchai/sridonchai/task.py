38c1b84df7594f10aaf906924594ede5a9e7631a
Chenz <64021799@up.ac.th>
2024-02-15 23:37:56 +0700

    update task

--- sridonchai/sridonchai/task.py
+++ sridonchai/sridonchai/task.py
@@ -2,29 +2,44 @@ import frappe
 def insert_meter_cost():
     date=frappe.utils.today()
     month=date[0:7]
-    #get a single doctype
+#get a single doctype
     doc = frappe.get_doc('Sridonchai config')
     doc.meter_maintenance_cost
     result = frappe.db.sql(
             f"""
-            left join `tabMeter Maintenance Cost` on `tabHouse`.name=`tabMeter Maintenance Cost`.house  
-            where active= 1 and `tabMeter Maintenance Cost`.name is null
+            select `tabHouse`.name from `tabHouse`
             """,
 
             as_dict=True
-        )
+    )
+    result2= frappe.db.sql(
+    
+            f"""
+            select house from`tabMeter Maintenance Cost`
+            left join `tabMonthly`on `tabMonthly`.name=`tabMeter Maintenance Cost`.month
+            where  `tabMeter Maintenance Cost`.month is not null and LPAD(month(`tabMeter Maintenance Cost`.transaction_date), 2, '0')=`tabMonthly`.month and year(transaction_date)=`tabMonthly`.year
 
+
+         
+            """,
+
+            as_dict=True
+    )
+    b=[j.house for j in result2]
     for i in result:
-        document_insert= frappe.get_doc({
-            'doctype': 'Meter Maintenance Cost',
-            'cost_paid': 5,
-            'house':i.name,
-            'transaction_date':date,
-            'month':month,
-        
+    
+    
+        if i.name not in b:
+            document_insert= frappe.get_doc({
+                'doctype': 'Meter Maintenance Cost',
+                'cost_paid': 5,
+                'house':i.name,
+                'transaction_date':date,
+                'month':month
             
+    })  
         
-        })
-        document_insert.insert()
+            document_insert.insert(ignore_permissions=True)
+            frappe.db.commit()
     return document_insert
     
